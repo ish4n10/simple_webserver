6995172a0ca3ce7a35465d2ea7aee4c0c8365d08
ish4n10 <ishan.tripathi010@gmail.com>
2023-12-02 16:27:44 +0530

    adding test data

--- src/main.rs
+++ src/main.rs
@@ -25,5 +25,10 @@ fn main() {
 fn handle_connections(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
+    let response = 
+    "HTTP/1.1 200 OK/\r\n\r\n";
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
